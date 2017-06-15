ce5ab4c041d2ab3cb7c349401eb1c5d616eca66e
Magnus Søndergaard Christensen <magn0811@edu.campusvejle.dk>
2017-06-15 14:28:22 +0200

    checkUser Added

--- checkUser.php
+++ checkUser.php
@@ -5,10 +5,12 @@ $formPassword = $_POST['formPassword'];
 
 // laver en forbindelse over til connect.php
 require_once "connect.php";
+
 // Hent de brugere ud som lige er blevet indtastet
 $statement = $DBH->prepare("SELECT * FROM users WHERE dbUsername=?");
 $statement->bindParam(1, $formUsername);
 $statement->execute();
+
 //hiver alle rækkerne ud af $statement
 $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
 
@@ -21,30 +23,28 @@ if(empty($rows)){
 
 // Vi kommer ind i else hvis brugernavn passer
 else{
-    //Der findes brugere med det samme brugernavn nu skal PW testes
+//Der findes brugere med det samme brugernavn nu skal PW testes
     //Kør et loop der tester hver række om dbPassword matcher formPassword
     //For hver række trækker vi en række ud og ser om den passer
     foreach($rows as $row){
-       
-        if($row['dbPassword'] == $formPassword){
-            
-            echo "SUCCES!!";
+        if($row['dbPassword'] == $formPassword){            
+        
             //starten ern session hvor man kan lagere forskellige værdier i
             //Session starter
             session_start();
             // i min session vil jeg lagere et username som skal være lig med det brugeren har indtastet
             $_SESSION['username'] = $formUsername;
-            
+                      
             //Hvis password også passer kommer vi tilbage til nyheder.php
             header("location: index.php");
+            
         }
     }
     //Kommer her ned hvis password ikke passer
     echo "<h1>Ikke korrekt password!<br>
     Prøv igen. Hvis du er en robot prøv ikke igen!</h1>";
     header("Refresh:3; url=loginsite.php");
-
 }
-$DBH = null;
 
+$DBH = null;
 ?>
