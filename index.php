11a99550370a6c06f1991cb1e5212d65667c1c9c
Magnus Søndergaard Christensen <magn0811@edu.campusvejle.dk>
2017-06-15 09:48:09 +0200

    File and folder upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <!-- Font-awesome -->
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <!-- Flexbox Grid -->
+    <link rel="stylesheet" href="css/flexboxgrid.css">
+    <!-- Normalize -->
+    <link rel="stylesheet" href="css/normalize.css">
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="css/style.css"> </head>
+
+<body> </body>
+
+</html>
